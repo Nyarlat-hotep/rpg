fc01c5bb7129ec1cf4588af52fb2af1af5910a03
Taylor Cornelius <tcornelius@brownbagmarketing.com>
2016-09-20 11:50:55 -0400

    Added function for selected class on abilities card. Added class for half progress meter.

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.controller('storeController', ["$scope", "classesService", "abilitiesService
     $scope.classes = classesService.classes;
     $scope.isDisabled = false;
     $scope.classOneFourthProgress = false;
+    $scope.isDisabled2 = false;
+    $scope.classHalfProgress = false;
 
     // function to re-retotal points. Sets disabled for buttons, progress class for progress meter
     $scope.calcCostController = function(classCost) {
@@ -16,12 +18,21 @@ app.controller('storeController', ["$scope", "classesService", "abilitiesService
         $scope.classOneFourthProgress = true;
     };
 
-    // function for active class on card
+    // function for active class card
     $scope.activeCard = function(index) {
         $scope.selectedCard = index;
         $scope.selectedCharacter = abilitiesService.allAbilities[index].abilities;  // get char at specific index
         $scope.pushedName = [];
         $scope.pushedName.push(classesService.classes[index].name); // add class name to footer
+    };
+
+    // function for active ability card
+    $scope.activeAbilityCard = function(index) {
+        $scope.selectedCard2 = index;
+        $scope.isDisabled2 = true;
+        $scope.classHalfProgress = true;
+        $scope.pushedAbility = [];
+        $scope.pushedAbility.push(abilitiesService.allAbilities[index].abilities[index].name);
     };
 
     // properties for naming character
@@ -81,7 +92,6 @@ app.service('classesService', function() {
         return total -= classCost;
     };
 
-    console.log(classesService.classes);
 
 });
 
@@ -110,25 +120,24 @@ app.service('abilitiesService', function() {
     barbarian.abilities.push(createAbility("Berserk", "A powerful attack that leaves the Barbarian more vulnerable.", 3, "https://hydra-media.cursecdn.com/diablo.gamepedia.com/c/c5/Berserk_Icon.png?version=8e96532155ffd518e66f04ca443084f8"));
 
     var mage = {'abilities':[]};
-    mage.abilities.push(createAbility("Fire Bolt", "Casts a small projectile of fire through the air and in to one enemy.", 1));
-    mage.abilities.push(createAbility("Nova", "When cast, creates an expanding ring of pure lightning damage to hurt surrounding enemies within a fixed range", 2));
-    mage.abilities.push(createAbility("Blizzard", "Gives a defense bonus and freezes any melee attacker that hits you.", 3));
-    mage.abilities.push(createAbility("Hydra", "Summons a Fire-elemental in the form of a three-headed, fire-spitting hydra.", 3));
-    // mage.abilities.splice(0, 4);
+    mage.abilities.push(createAbility("Fire Bolt", "Casts a small projectile of fire through the air and in to one enemy.", 1, "https://hydra-media.cursecdn.com/diablo.gamepedia.com/6/68/Fire_Bolt_Icon.png?version=ea538224a6502a40ee0f20f4dde1932c"));
+    mage.abilities.push(createAbility("Nova", "When cast, creates an expanding ring of pure lightning damage to hurt surrounding enemies within a fixed range", 2, "https://hydra-media.cursecdn.com/diablo.gamepedia.com/8/89/Nova_Icon.png?version=d3715978b0c4b9bdd7fb0d5e708b04ff"));
+    mage.abilities.push(createAbility("Blizzard", "Gives a defense bonus and freezes any melee attacker that hits you.", 3, "https://hydra-media.cursecdn.com/diablo.gamepedia.com/4/4b/Blizzard_Icon.png?version=a5372a61a1d0ec27d2d0453495e914d3"));
+    mage.abilities.push(createAbility("Hydra", "Summons a Fire-elemental in the form of a three-headed, fire-spitting hydra.", 3, "https://hydra-media.cursecdn.com/diablo.gamepedia.com/3/3b/Hydra_Icon.png?version=18d3fd84ee9a2572242dcddd61c8fde2"));
 
     var paladin = {'abilities':[]};
-    paladin.abilities.push(createAbility("Paladin", "Might", "Increases damage dealt by party members.", 1));
-    paladin.abilities.push(createAbility("Thorns", "Enemies take damage when they cause melee damage to party members.", 2));
-    paladin.abilities.push(createAbility("Holy Shock", "Periodically does Lightning damage to enemies within a radius.", 3));
-    paladin.abilities.push(createAbility("Conviction", "Reduces enemy Defense and the Resistances of monsters.", 3));
+    paladin.abilities.push(createAbility("Might", "Increases damage dealt by party members.", 1, "https://hydra-media.cursecdn.com/diablo.gamepedia.com/8/88/Might_Icon.png?version=f1f6368f162d8b3ad64e402cc0530edf"));
+    paladin.abilities.push(createAbility("Thorns", "Enemies take damage when they cause melee damage to party members.", 2, "https://hydra-media.cursecdn.com/diablo.gamepedia.com/9/90/Thorns_Icon.png?version=db66989fa7cddd342926bc5cbd53d8ff"));
+    paladin.abilities.push(createAbility("Holy Shock", "Periodically does Lightning damage to enemies within a radius.", 3, "https://hydra-media.cursecdn.com/diablo.gamepedia.com/c/c3/Holy_Shock_Icon.png?version=5d099d421643a689bdb7e356c19ee3a6"));
+    paladin.abilities.push(createAbility("Conviction", "Reduces enemy Defense and the Resistances of monsters.", 3, "https://hydra-media.cursecdn.com/diablo.gamepedia.com/8/81/Conviction_Icon.png?version=542eec1f0dcc0b13cf83807c839d99d6"));
     // paladin.abilities.splice(0, 4);
     // console.log(paladin);
 
     var assassin = {'abilities':[]};
-    assassin.abilities.push(createAbility("Dragon Talon", "An Assassin is taught to utilize her entire body as a weapon using this skill, she lets loose a powerful kick to send her opponents flying.", 1));
-    assassin.abilities.push(createAbility("Cobra Strike", "A properly trained Assassin can focus her mind to draw upon the ambient energies surrounding her. Using this skill, she can drain her adversary of life and spiritual essence.", 2));
-    assassin.abilities.push(createAbility("Mind Blast", "Focusing her anima, an Assassin using this potent ability can crush the will of a group of enemies, stunning them and confusing the feebleminded into attacking their comrades.", 3));
-    assassin.abilities.push(createAbility("Death Sentry", "This trap emits projectiles laden with a potent chemical catalyst, detonating the exposed cadavers of slain enemies. Damage: 40-80% Of Corpse Life. Shoots 5 times.", 3));
+    assassin.abilities.push(createAbility("Dragon Talon", "An Assassin is taught to utilize her entire body as a weapon using this skill, she lets loose a powerful kick to send her opponents flying.", 1, "https://hydra-media.cursecdn.com/diablo.gamepedia.com/a/a4/Dragon_Talon_Icon.png?version=d766f12028e4a1b48f31fa31a12c1376"));
+    assassin.abilities.push(createAbility("Cobra Strike", "A properly trained Assassin can focus her mind to draw upon the ambient energies surrounding her. Using this skill, she can drain her adversary of life and spiritual essence.", 2, "https://hydra-media.cursecdn.com/diablo.gamepedia.com/8/85/Cobra_Strike_Icon.png?version=a43170b2cf19a719980d94a0e6d4ac41"));
+    assassin.abilities.push(createAbility("Mind Blast", "Focusing her anima, an Assassin using this potent ability can crush the will of a group of enemies, stunning them and confusing the feebleminded into attacking their comrades.", 3, "https://hydra-media.cursecdn.com/diablo.gamepedia.com/c/c4/Mind_Blast_Icon.png?version=ecdb53dc0353a321d870034866b8a2a8"));
+    assassin.abilities.push(createAbility("Death Sentry", "This trap emits projectiles laden with a potent chemical catalyst, detonating the exposed cadavers of slain enemies. Damage: 40-80% Of Corpse Life. Shoots 5 times.", 3, "https://hydra-media.cursecdn.com/diablo.gamepedia.com/f/fd/Death_Sentry_Icon.png?version=ea1ec08af7981f41cf8d7fe39d422fe4"));
     // assassin.abilities.splice(0, 4);
 
 
